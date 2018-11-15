d26cfdf6c1d5772032d959f906403632f9ab93d8
Kieran Burns <kieran.burns@yahoo.com>
2018-11-15 19:41:47 +0000

    Add files via upload

--- sourcecode/mainApp.py
+++ sourcecode/mainApp.py
@@ -2,7 +2,8 @@ import sqlite3
 import bcrypt
 import os
 import os.path
-from functools import wraps
+import random
+from array import *
 from flask import Flask, render_template, redirect, request, session, url_for
 
 app = Flask(__name__)
@@ -14,14 +15,41 @@ app.secret_key = 'asdfghjkkl;;'
 
 
 def checkLogged():
-	print session['logged']
-	if session['logged'] == True:
-		return True
-	return False
+	try:
+		if session['logged'] == True:
+			return True
+		return False
+	except:
+		return False
 
 @app.route('/')
 def home():
 	return render_template('home.html', logged=checkLogged())
+
+@app.route('/', methods=['POST'])
+def home_post():
+	link = None
+	conn = sqlite3.connect(urlData)
+	cursor = conn.cursor()
+	chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','$','_','-','!'] 
+	while (link == None):
+		temp = ""
+		for x in range(6):
+			temp += chars[random.randint(0,39)]		
+
+		cursor.execute(""" SELECT compressed
+				   FROM userURLs
+				   WHERE compressed=?""",
+			       (temp,))
+		result = cursor.fetchone()
+		if not result:
+			link = temp
+
+	original = request.form['rawURL']
+	cursor.execute("INSERT INTO userURLs VALUES (?, ?, ?, ?)",(link, original, session['user'], 0))	
+	conn.commit()
+
+	return render_template('home.html', logged=checkLogged(), link=link) 
 
 @app.route('/signup')
 def signup():
@@ -30,7 +58,7 @@ def signup():
 @app.route('/signup', methods=['POST'])
 def signup_post():
 	error = None
-	email = request.form['email']
+	username = request.form['username']
 	password = request.form['password']
 	password2 = request.form['password2']
 	
@@ -40,17 +68,18 @@ def signup_post():
 	else:
 		conn = sqlite3.connect(userData)
 		cursor = conn.cursor()
-		cursor.execute(""" SELECT email
+		cursor.execute(""" SELECT username
 				   FROM login
-				   WHERE email=?""",
-			       (email,))
+				   WHERE username=?
+				   COLLATE NOCASE""",
+			       (username,))
 		result = cursor.fetchone()
 		if result:
-			error = "Account using input Email aready exists!"
+			error = "Account using input Username aready exists!"
 			return render_template('signup.html', logged=checkLogged(), error=error)
 		else:
 			hash = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-			cursor.execute("INSERT INTO login VALUES (?, ?)", (email, hash))
+			cursor.execute("INSERT INTO login VALUES (?, ?)", (username, hash))
 			conn.commit()
 			return redirect('login')
 
@@ -61,34 +90,31 @@ def login():
 @app.route('/login', methods=['POST'])
 def login_post():
 	error = None
-	email = request.form['email']
+	username = request.form['username']
 	password = request.form['password']
 	
 	conn = sqlite3.connect(userData)
 	cursor = conn.cursor()
-	cursor.execute(""" SELECT email
+	cursor.execute(""" SELECT username
 			   FROM login
-			   WHERE email=?""",
-		       (email,))
+			   WHERE username=?""",
+		       (username,))
 	result = cursor.fetchone()
 	if result:
 		cursor.execute(""" SELECT password
 				    FROM login
-				    WHERE email=?""",
-			        (email,))
+				    WHERE username=?""",
+			        (username,))
 
 		readPW = cursor.fetchone()[0].encode('utf8')
 		
 		if readPW == bcrypt.hashpw(password.encode('utf8'), readPW):
-			session['user'] = email
+			session['user'] = username
 			session['logged'] = True
-			return redirect('/')			
-		else:
-			error = "Incorrect Password"
-			return render_template('login.html', logged=checkLogged(), error=error) 
-	else:
-		error = "No account of EMAIL exists!"
-		return render_template('login.html', logged=checkLogged(), error=error)	
+			return redirect('/')
+				
+	error = "Invalid Login details!"
+	return render_template('login.html', logged=checkLogged(), error=error)	
 
 
 @app.route('/logout')
@@ -100,6 +126,87 @@ def logout():
 @app.route('/mylinks')
 def mylinks():
 	if not session['logged']:
-		return redirect('/signup')
+		return redirect('/login')
+	else:
+		conn = sqlite3.connect(urlData)
+		cursor = conn.cursor()
+		cursor.execute(""" SELECT * 
+				   FROM userURLs
+  				   """
+			       )
+		rows = cursor.fetchall()
+		myURLs = []
+		total = 0
+		for row in rows:
+			temp = []
+			temp.append(row[0])
+			temp.append(row[1])
+			temp.append(row[3])
+			myURLs.append(temp)
+			total+=1
+		if total == 0:
+			return render_template('mylinks.html', logged=checkLogged())
+		else:	
+			return render_template('mylinks.html', logged=checkLogged(),myURLs=myURLs,total=total)
+
+@app.route('/myaccount')
+def myaccount():
+	if not session['logged']:
+		return redirect('/login')
 	else:
 		return session['user']
+
+@app.route('/url/<url>')
+def url(url):
+	conn = sqlite3.connect(urlData)
+	cursor = conn.cursor()
+	cursor.execute(""" SELECT original
+			   FROM userURLs
+			   WHERE compressed=?
+			   COLLATE NOCASE""",
+		       (url,))
+
+	result = cursor.fetchone()
+	
+	if result:
+		cursor.execute(""" SELECT clicks
+				   FROM userURLs
+				   WHERE compressed=?
+				   COLLATE NOCASE""",
+			       (url,))
+		clicks = cursor.fetchone()[0]
+		clicks += 1
+		cursor.execute(""" UPDATE userURLs
+				   SET clicks=?
+				   WHERE compressed=?
+				   COLLATE NOCASE""",
+                               (clicks,url,))
+		conn.commit()
+		return redirect(result[0])		
+	else:
+		return "!!!!!!!!!!!!!!!!!!!!!!!!!!!!FAIL PAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+
+@app.route('/del/<url>')
+def delurl(url):
+	logged = checkLogged()
+	if logged:
+		conn=sqlite3.connect(urlData)
+		cursor = conn.cursor()
+		cursor.execute(""" SELECT owner
+				   FROM userURLs
+				   WHERE compressed=?
+				   COLLATE NOCASE""",
+			       (url,))
+		result = cursor.fetchone()
+
+		if result:
+			if result[0] == session['user']:
+				cursor = conn.cursor()
+				cursor.execute(""" DELETE FROM userURLs
+                        	                   WHERE compressed=?
+                        	                   COLLATE NOCASE""",
+                        	               (url,))
+				conn.commit()
+				return redirect('/mylinks')
+	return "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!FAIL PAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
